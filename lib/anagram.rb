7c0a9110b72e9db59a649afb0513369eed4a9c1c
Hunter Sides <hunter.s.1337@gmail.com>
2020-02-27 21:07:53 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,5 +8,19 @@ class Anagram
   
   def new_match
     new_array = @word.match(word)
-    
+  end 
+  
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_array)
+    word_array.select do |word|
+      word.split("").sort == @word.split("").sort
+    end
+  end
+end
    

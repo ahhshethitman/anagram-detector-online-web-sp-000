682536a249e8cdff779b4f6a20ec21d367a042a9
Hunter Sides <hunter.s.1337@gmail.com>
2020-02-27 21:09:07 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,14 +1,6 @@
 # Your code goes here!
 
-class Anagram 
-  attr_accessor = :word
-  def initialize(word)
-    @word = word 
-  end 
-  
-  def new_match
-    new_array = @word.match(word)
-  end 
+
   
 class Anagram
   attr_accessor :word
